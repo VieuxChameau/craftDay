502a91d8dafd2a192924cae05d02ef79f9c6bd9d
VieuxChameau <VieuxChameau@users.noreply.github.com>
2014-09-29 22:31:35 +0200

    Ajout repertoire test et suppression de la contrainte sur la version java

---
+++ src/test/java/com/berage/BeverageDispenserTest.java
@@ -0,0 +1,14 @@
+package com.berage;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BeverageDispenserTest {
+    @Test
+    public void testNothing() {
+        assertThat(Boolean.FALSE).isFalse();
+    }
+}
